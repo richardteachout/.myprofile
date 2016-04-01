dbf7017a126bc82ac9b6e1fb57d1d44d86178bb9
rich <rich@eltoro.com>
2016-04-01 09:39:22 -0400

    shortcut file

--- initialize.sh
+++ initialize.sh
@@ -3,7 +3,7 @@
 cd ~
 echo -n "working on setting up directory:"
 pwd
-for i in .bash_aliases .bashrc .inputrc .tmux.conf .vimrc
+for i in .bash_aliases .bashrc .inputrc .tmux.conf .vimrc .bash_shortcuts
 do
 rm $i -f
 ln -s .myprofile/$i $i
