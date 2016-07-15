21d70bf495de678d0528f04f34a8476d5876f13c
Richard Teachout <rich@eltoro.com>
2016-07-15 13:03:30 -0400

    updated install to mask vim screen

--- Scripts/install-home.sh
+++ Scripts/install-home.sh
@@ -3,6 +3,6 @@ git clone https://github.com/richardteachout/.myprofile.git
 mv .myprofile/* ./ && mv .myprofile/.[a-z]* ./ && rmdir .myprofile
 git remote add write git@github.com:richardteachout/.myprofile.git
 git submodule update --init --recursive
-vim +BundleInstall +qall
+vim +BundleInstall +qall  2&> /dev/null
 #zsh
 #chsh -s /bin/zsh
